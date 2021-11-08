d3f2adaf58a8bf252382e949a88c8c8a201cc8f0
RebekahJulicher <70235428+RebekahJulicher@users.noreply.github.com>
2021-11-07 21:25:13 -0700

    Define/set var functions, intEval/boolEval starts

--- src/Parser.java
+++ src/Parser.java
@@ -9,8 +9,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
-	static Set<Pattern> patterns;
-	static Map<String, String> vars;
+	static Map<String, Pattern> patterns;
+	static ArrayList< Map<String, VarContent> > vars;
 	
 	public static void main(String[] args) {
 		patterns = new HashSet<Pattern>();
@@ -30,8 +30,128 @@ public class Parser {
 		}
 	}
 	
+	private static void defineVar(String line){
+		String[] strArray = line.split(" ");
+		for (Map<String, VarContent) x : vars){
+			if (x.get(strArray[1]) != null){
+				System.out.println("SYNTAX ERROR: Var " + strArray[1] + " has already been" +
+									"defined, cannot define it again!");
+				e.printStackTrace();
+				System.exit();
+			}
+		}
+		Matcher m = patterns.get("bool_expr").matcher(strArray[3]);
+		if (m.matches()){
+			vars.get(vars.size() - 1).add(strArray[1], new VarContent(boolEval(strArray[3]));
+			return;
+		}
+		m = patterns.get("i_expr").matcher(strArray[3]);
+		if (m.matches()){
+			vars.get(vars.size() - 1).add(strArray[1], new VarContent(intEval(strArray[3]));
+			return;
+		}
+		m = patterns.get("string").matcher(strArray[3]);
+		if (m.matches()){
+			vars.get(vars.size() - 1).add(strArray[1],
+						new VarContent(strArray[3].substring(0, strArray.length()-1));
+			return;
+		}
+		// Checking if we're defining var in terms of another var
+		for (Map<String, VarContent) x : vars){
+			if (x.get(strArray[3]) != null){
+				// THIS IS ASSUMING WE WANT TO DO COPIES INSTEAD OF POINTERS FOR THIS
+				VarContent original = x.get(strArray[3]);
+				if (original.type == 0) vars.get(vars.size() - 1).add(strArray[1],
+													new VarContent(original.integerVal));
+				else if (original.type == 1) vars.get(vars.size() - 1).add(strArray[1],
+														new VarContent(original.boolVal));
+				else vars.get(vars.size() - 1).add(strArray[1], new VarContent(original.stringVal));
+				return;
+			}
+		}
+		// TODO: ERROR OUT
+	}
+
+	private static void setVar(String line){
+		String[] strArray = line.split(" ");
+		VarContent content = vars.get(vars.size()-1).get(strArray[0])''
+		
+		if (content == null){
+			System.out.println("SYNTAX ERROR: Var " + strArray[0] + " has not been" +
+									"defined, cannot set it!");
+			e.printStackTrace();
+			System.exit();
+		}
+		Matcher m = patterns.get("bool_expr").matcher(strArray[2]);
+		if (m.matches()){
+			if (content.type != 1){
+				System.out.println("SYNTAX ERROR: Var " + strArray[0] + " is not of" + 
+									"type boolean, so it cannot be set to a boolean!");
+				e.printStackTrace();
+				System.exit();
+			}
+			content.boolVal = boolEval(strArray[2]);
+			return;
+		}
+		m = patterns.get("i_expr").matcher(strArray[2]);
+		if (m.matches()){
+			if (content.type != 0){
+				System.out.println("SYNTAX ERROR: Var " + strArray[0] + " is not of" +
+									"type int, so it cannot be set to an int!");
+				e.printStackTrace();
+				System.exit();
+			}
+			content.integerVal = intEval(strArray[2]);
+			return;
+		}
+		m = patterns.get("string").matcher(strArray[2]);
+		if (m.matches()){
+			if (content.type != 0){
+				System.out.println("SYNTAX ERROR: Var " + strArray[0] + " is not of " +
+									"type string, so it cannot be set to a string!");
+				e.printStackTrace();
+				System.exit();
+			}
+			content.integerVal = strArray[3].substring(0, strArray.length()-1);
+			return;
+		}
+		// Checking if we're defining var in terms of another var
+		for (Map<String, VarContent) x : vars){
+			if (x.get(strArray[2]) != null){
+				// THIS IS ASSUMING WE WANT TO DO COPIES INSTEAD OF POINTERS FOR THIS
+				VarContent original = x.get(strArray[2]);
+				if (original.type == content.type){
+					if (original.type == 0) content.integerVal = original.integerVal;
+					else if (original.type == 1) content.boolVal = original.boolVal;
+					else content.stringVal = original.stringVal;
+					return;
+				}
+				System.out.println("SYNTAX ERROR: Var " + strArray[0] + " is not of the " +
+									"sametype as var " + strArray[2]);
+				e.printStackTrace();
+				System.exit();
+			}
+		}
+		// TODO: ERROR OUT
+	}
+
+	private static int intEval(String expr){
+		if (expr.isdigit()) return Integer.valueOf(expr);
+		if (expr.lstrip('-').isdigit()) return 0 - Integer.valueOf(expr);
+		// TODO: add recursive expression evaluating;
+	}
+
+	// Pass in a boolean or boolean expression
+	private static boolean boolEval(String expr){
+		if (expr.equals("true")) return true;
+		if (expr.equals("false")) return false;
+		// TODO: add recursive expression evaluating
+	}
 
 	private static void setUpPatterns() {
+		String comment = "$+[\\d*[\\w&&[^\\d]]*]*";
+		String string = '"' + "[\\d*[\\w&&[^\\d]]*]*" + '"';
+
 		String digit = "\\d";
 		String bool = "true|false";
 		Pattern integer = Pattern.compile(digit + "+");
@@ -40,6 +160,7 @@ public class Parser {
 		String commandLineArg = "$arg[" + integer + "]";
 		
 		String variable = "[\\w&&[^\\d]]+[\\d*[\\w&&[^\\d]]*]*";
+		// value is going to need bool_expr, i_expr, and string
 		String value = "(" + integer + "|" + bool + "|" + commandLineArg + "|" + variable + ")";
 		
 		Pattern var_assgn = Pattern.compile("var " + variable + " = " + value);
@@ -77,4 +198,27 @@ public class Parser {
 		
 		// output translation to file
 	}
+
+	static class VarContent {
+		// 0 for int, 1 for boolean, 2 for string
+		int type;
+		int integerVal;
+		boolean boolVal;
+		String stringVal;
+		public VarNode(int val){
+			type = 0;
+			integerVal = val;
+		}
+
+		public VarNode(boolean val){
+			type = 1;
+			boolVal = val;
+		}
+
+		public VarNode(String val){
+			type = 2;
+			stringVal = val;
+		}
+	}
+
 }
